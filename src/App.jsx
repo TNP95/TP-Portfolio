925cf9505f46387914172963973426a3db9cdc2f
Tiago Pinto <tiiagopinto@gmail.com>
2023-07-03 00:54:03 +0100

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./assets/componentes/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="bg-red-500">
       <Header />
 
       <div className="flex justify-center mt-20">
